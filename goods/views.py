600e9d53401bd112fcdb50e3c54c6fab4ca0096e
Viktor6109 <viktor.kosyura@ukr.net>
2026-02-15 22:08:57 +0100

    add pagination

--- goods/views.py
+++ goods/views.py
@@ -1,41 +1,52 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from goods.models import Products
 
 
-# Create your views here.
 def catalog(request, category_slug):
-    page: int = request.GET.get('page', 1)
-    on_sale: bool = request.GET.get('on_sale', None)
-    order_by = request.GET.get('order_by', None)
-
-    if category_slug == 'all':
-        goods = Products.objects.all()
+    if category_slug == "all":
+        goods = Products.objects.all().select_related('category')
     else:
-        goods = Products.objects.filter(category__slug=category_slug)
+        goods = Products.objects.filter(category__slug=category_slug).select_related('category')
 
-    if on_sale:
+    # Применяем фильтры и сортировку
+    on_sale = request.GET.get("on_sale")
+    if on_sale == "on":
         goods = goods.filter(discount__gt=0)
 
-    if order_by and order_by != "default":
-        goods=goods.order_by(order_by)
+    order_by = request.GET.get("order_by", "default")
+    if order_by == "price":
+        goods = goods.order_by("price")
+    elif order_by == "-price":
+        goods = goods.order_by("-price")
+    else:
+        goods = goods.order_by("id")  # Сортировка по умолчанию (по id)
 
+    # Пагинация
+    page = request.GET.get("page", 1)
     paginator = Paginator(goods, 3)
     current_page = paginator.get_page(page)
 
+   
+    # Сохраняем остальные параметры для пагинации
+    params = request.GET.copy()
+    params.pop("page", None)
+    params = params.urlencode()
+
     context = {
-        'title': 'Продукция',
-        'goods': current_page,
-        'slug_url': category_slug,
+        "title": "Продукция",
+        "goods": current_page,
+        "slug_url": category_slug,
+        "params": params,
     }
 
-    return render(request, 'goods/catalog.html', context)
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
     context = {
-        'product': product,
+        "product": product,
     }
-    return render(request, 'goods/product.html', context)
+    return render(request, "goods/product.html", context)
