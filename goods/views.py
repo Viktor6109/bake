79f55e60f2c972d48891f2e5d309e8ad5a4495e3
Viktor6109 <viktor.kosyura@ukr.net>
2026-02-15 22:07:25 +0100

    Add new SVG icons and images; update base template structure
    - Added new SVG icons: google, grid-fill, trash3-fill, basket2-fill, box-arrow-in-left, box-arrow-in-right, cart-plus, facebook, github.
    - Added new images: Not found image, baseavatar, bg-image, various goods images (bedside table, corner sofa, double bed, flower, kitchen table, office chair, plants, tea table sets, sofa, strange table).
    - Updated base.html to improve structure and accessibility of the catalog and cart modal.
    - Enhanced cart modal functionality with dynamic item count and total price display.

--- goods/views.py
+++ goods/views.py
@@ -1,20 +1,40 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from goods.models import Products
 
 
 # Create your views here.
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    page: int = request.GET.get('page', 1)
+    on_sale: bool = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods=goods.order_by(order_by)
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.get_page(page)
+
     context = {
         'title': 'Продукция',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': category_slug,
     }
+
     return render(request, 'goods/catalog.html', context)
 
 
 def product(request, product_slug):
-    product = Products.objects.get(slug = product_slug)
+    product = Products.objects.get(slug=product_slug)
     context = {
         'product': product,
     }
